86638bff225e620f711c19d0a540e153353cba25
sooraj.cs <sooraj.cs@cogknit.com>
2024-05-29 22:39:16 +0530

    changed BE port

--- chat-ui-template/src/store/chats.js
+++ chat-ui-template/src/store/chats.js
@@ -97,7 +97,7 @@ const chatsStore = (set, get) => ({
 
     async getReply(msg, replyId) {
       try {
-        await fetchEventSource("http://localhost:5000/api/generate", {
+        await fetchEventSource("http://localhost:8081/api/generate", {
           method: "POST",
           headers: {
             Accept: "text/event-stream",
